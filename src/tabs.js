4caa2e3cb69bc0320af6a6a352323ed124d4d953
shanikweb <shanik@wearerecess.ca>
2025-09-08 11:36:20 -0400

    homepage fix

---
+++ src/tabs.js
@@ -0,0 +1,13 @@
+export const tabs = [
+  {
+    title: "Nobody Has A Personality Anymore",
+    url: "https://freyaindia.co.uk",
+    summary: "You are the way you are not because you have a soul but because of your symptoms and diagnoses; you are not an amalgam of your ancestors or curious constellation of traits but the clinical result of a timeline of childhood events...",
+  },
+  {
+    title: "The Death of the Middle-Class Restaurant",
+    url: "https://nytimes.com",
+    summary: "Once rapidly growing commercial marvels, casual dining chains — sit-down restaurants where middle-class families can walk in without a reservation, order from another human and share a meal — have been in decline for most of the 21st century...",
+  },
+  // Add more tabs here!
+];
